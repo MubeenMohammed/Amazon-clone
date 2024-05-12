3ef3562cd239adfdd22b863f998dc6028ab18602
MubeenMohammed <mubeen.mohammed@mail.mcgill.ca>
2024-05-11 20:30:32 -0400

    saving the cart in local storage

--- data/cart.js
+++ data/cart.js
@@ -1,10 +1,15 @@
-export let cart  = [{
+export let cart  = JSON.parse(localStorage.getItem('cart')) || [{
     productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
     quantity: 2
 }, {
     productId: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
     quantity:1
 }];
+
+
+export function saveToStorage() {
+    localStorage.setItem('cart', JSON.stringify(cart));
+}
 
 export function addToCart(productId) {
     let matchingItem;
@@ -26,7 +31,7 @@ export function addToCart(productId) {
         });
     }
 
-    console.log(cart);
+    saveToStorage();
 }
 
 export function removeFromCart(productId) {
@@ -37,4 +42,5 @@ export function removeFromCart(productId) {
         }
     });
     cart = newCart;
+    saveToStorage();
 };
